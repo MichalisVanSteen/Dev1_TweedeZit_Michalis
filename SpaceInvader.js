8e9205cf40a3fdde9db3e443574d3f506bf34905
Michalis Van Steen <michalis.van.steen@student.ehb.be>
2020-07-25 12:36:37 +0200

    Afwerking en testen Taak 1 Space Invader

--- SpaceInvader.js
+++ SpaceInvader.js
@@ -1,8 +1,32 @@
 'use strict';
-import context from "./context/context.js";.
+import context from "./context/context.js";
 
 function drawSpaceInvader() {
-    context.strokeRect = "green";
+
+
+
     context.strokeRect(50, 50, 300, 300);
-    context.fillRect("black");
+    context.fillRect(50, 50, 300, 300);
+    context.fill();
+    context.stroke();
+
+    context.fillStyle = "green";
+    //Groot balk bovenstuk
+    context.rect(75, 125, 250, 50);
+    //Kleine kubus links boven
+    context.rect(125, 75, 50, 50);
+    //Kleine kubus rechts boven
+    context.rect(225, 75, 50, 50);
+    //Kleine kubus midden onder
+    context.rect(175, 175, 50, 50);
+    //Middelgrote balk links onder
+    context.rect(75, 225, 50, 100);
+    //Middelgrote balk rechts onder
+    context.rect(275, 225, 50, 100);
+    //Kleine kubus minder onderaan
+    context.rect(175, 275, 50, 50);
+    context.fill();
+
 }
+
+drawSpaceInvader();
